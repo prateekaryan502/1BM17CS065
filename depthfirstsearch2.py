1e30ffa39734ddbbd0faff9698333a992ac7537f
Prateek Aryan <31097733+prateekaryan502@users.noreply.github.com>
2019-09-04 13:17:30 +0530

    Update depthfirstsearch2.py

--- depthfirstsearch2.py
+++ depthfirstsearch2.py
@@ -1,6 +1,5 @@
-matrix=[[1,0,1,0,1],[1,1,1,1,0],[0,1,1,1,1],[0,1,1,1,1],[1,0,1,0,0]]
-for i in matrix:
-    print(i)
+matrix=[[1,0,1,0,1],[1,1,0,0,0],[0,1,0,1,1],[0,0,0,0,0],[0,0,0,0,0]]#,[0,1,1,1,1],[1,0,1,0,0]]
+
 count =0
 visited=[]
 for i in range(len(matrix)):
@@ -8,17 +7,17 @@ for i in range(len(matrix)):
     for j in range(len((matrix[i]))) :
         visited[i].append(False)
 
-print(visited)
+
 def DFS(matrix,i,j,visited):
-    print(i,j,count)
+
     visited[i][j]=True
     for m in range(i-1,i+2):
         for n in range(j-1,j+2):
-            if m-1 >= 0 and n - 1 >= 0 and m+1 < len(matrix) and n + 1 <= len(matrix) and visited[m][n]==False:
-                print(m,n,"in")
+            if m>= 0 and n>= 0 and m < len(matrix) and n < len(matrix) and visited[m][n]==False and matrix[m][n]==1:
+
                 DFS(matrix,m,n,visited)
-            else:
-                print(m,n,"not in")
+
+
 for i in range(len(matrix)):
     for j in range(len(matrix)):
         if matrix[i][j]==1 and visited[i][j]==False :
@@ -26,4 +25,3 @@ for i in range(len(matrix)):
             count+=1
 
 print(count)
-print(visited)
